c91db57c8e57ed7e026aec68b71fc53ad0e18cf9
LauraPrichici <p_laura_c@yahoo.com>
2016-10-27 11:14:53 -0700

    The login button is not shown anymore in case the user is already logged in.

--- TwitterGrabber/ViewController.swift
+++ TwitterGrabber/ViewController.swift
@@ -12,22 +12,23 @@ import TwitterKit
 
 class ViewController: UIViewController {
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
+    private var isSessionActive: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         let store = Twitter.sharedInstance().sessionStore
         
-        if let userID = store.session()?.userID {
-            print("user is logged in with id \(userID)")
-            self.performSegue(withIdentifier: "showTabBarController", sender: self)
+        if let _ = store.session()?.userID {
+            isSessionActive = true
         }
         else
         {
-            print("user is not logged in")
             let logInButton = TWTRLogInButton { (session, error) in
                 if let _ = session {
+                    self.isSessionActive = true
                     self.performSegue(withIdentifier: "showTabBarController", sender: self)
                 } else {
+                    self.isSessionActive = false
                     print("Login error: \(error!.localizedDescription)");
                 }
             }
@@ -36,7 +37,15 @@ class ViewController: UIViewController {
             logInButton.center = self.view.center
             self.view.addSubview(logInButton)
         }
-        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+       if isSessionActive
+       {
+        self.performSegue(withIdentifier: "showTabBarController", sender: self)
+        }
     }
 
 }
